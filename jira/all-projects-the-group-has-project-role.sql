2d83198b370b7e3f4be4086ff50215a05bc282b4
Totally Tot <fcukthepop@gmail.com>
2018-11-12 17:32:14 +0300

    Update all-projects-the-group-has-project-role.sql

--- jira/all-projects-the-group-has-project-role.sql
+++ jira/all-projects-the-group-has-project-role.sql
@@ -1,4 +1,6 @@
 /*DB: PostgreSQL*/
+
 select p.pkey from project p
 join projectroleactor pra on p.id = pra.pid
-where pra.ROLETYPEPARAMETER = 'jira-software-users';
+where pra.ROLETYPEPARAMETER = 'jira-software-users'
+order by pkey;
