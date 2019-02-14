806e9e81be80f02dce7798c9f25ee03159154fce
Totally Tot <fcukthepop@gmail.com>
2019-02-14 18:25:17 +0300

    Update find-certain-content-in-pages.sql

--- confluence/find-certain-content-in-pages.sql
+++ confluence/find-certain-content-in-pages.sql
@@ -4,3 +4,18 @@ where CONTENTID in
 (select CONTENTID from CONTENT where SPACEID is not null and CONTENT_STATUS = 'current')
 and BODY like '%place your content here%'
 order by CONTENTID;
+
+//Mysql
+select CONCAT('https://wiki.example.com/pages/viewpage.action?pageId=', bc.CONTENTID) as 'Link', c.TITLE, s.SPACEKEY, s.SPACENAME, c.CONTENT_STATUS, c.VERSION, 
+um.username as Creator, c.CREATIONDATE, umm.username as 'Last Modifier', c.LASTMODDATE from bodycontent bc
+left join content c 
+on c.CONTENTID = bc.CONTENTID
+left join spaces s
+on c.SPACEID = s.SPACEID
+left join user_mapping um
+on um.user_key = c.CREATOR
+left join user_mapping umm
+on umm.user_key = c.LASTMODIFIER  
+where s.SPACEKEY not in ('SED2', 'SEUD2', 'SELD', 'PUBSELD', 'IWI', 'PB') 
+and c.CONTENTTYPE = 'PAGE' and bc.BODY like '%pswd%' COLLATE utf8_general_ci
+order by s.SPACEKEY;
