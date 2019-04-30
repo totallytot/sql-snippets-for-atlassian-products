f5171f6572cec9be26caec6f465790698abc8573
Totally Tot <fcukthepop@gmail.com>
2019-04-30 11:06:38 +0300

    Update password-audit.sql

--- jira/password-audit.sql
+++ jira/password-audit.sql
@@ -1,5 +1,5 @@
 //custom field
-select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.clicksoftware.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
+select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.example.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
 ji.creator as 'Creator', ji.created as 'Creation Date', ji.updated as 'Last Update Date', 
 cf.cfname as 'Custom Field', 'password' as 'Searched Value', p.lead as 'Project Lead' from customfieldvalue cfv
 left join jiraissue ji
@@ -12,7 +12,7 @@ where textvalue like '%password%' COLLATE utf8_general_ci
 order by p.pname;
 
 //system field
-select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.clicksoftware.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
+select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.example.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
 ji.creator as 'Creator', ji.created as 'Creation Date', ji.updated as 'Last Update Date', 'System Field', 'password' as 'Searched Value', p.lead as 'Lead'
 from jiraissue ji
 left join project p
@@ -21,7 +21,7 @@ where ji.description like '%password%' COLLATE utf8_general_ci
 order by p.pname;
 
 //comments
-select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.clicksoftware.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
+select p.pname as 'Project', concat(p.pkey, '-', ji.issuenum) as 'Issue Key', concat('https://jira.example.com/browse/', p.pkey, '-', ji.issuenum) as 'Link',
 ja.author, ja.created, ja.updateauthor, ja.updated, 'password', p.lead from jiraaction ja
 left join jiraissue ji on ji.id = ja.issueid
 left join project p on p.id = ji.project 
