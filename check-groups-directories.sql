f4a987195f8558d466af4f1e549ada087ae769de
Totally Tot <fcukthepop@gmail.com>
2019-06-07 17:07:23 +0300

    Delete check-groups-directories.sql

--- check-groups-directories.sql
+++
@@ -1,8 +0,0 @@
-/*Report that shows in which dirs groups are located.
-DB: Microsoft SQL Server*/
-
-SELECT cg.id, cg.group_name, cg.directory_id, cd.directory_name, cd.directory_type
-FROM cwd_group cg
-  JOIN cwd_directory cd
-  ON cg.directory_id = cd.id
-ORDER BY directory_name;
