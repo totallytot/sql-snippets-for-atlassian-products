d81f940e81540949f6d8be71bb5d637f59b959c7
Totally Tot <fcukthepop@gmail.com>
2019-07-19 08:23:52 +0300

    Update db-and-role-creation-for-postgresql.sql

--- db-and-role-creation-for-postgresql.sql
+++ db-and-role-creation-for-postgresql.sql
@@ -5,7 +5,7 @@ alter database jiradb owner to jira;
 create role confluence with password 'j76Tre!' login;
 CREATE DATABASE "confluence"
     WITH OWNER "confluence"
-    ENCODING 'UTF8'
+    ENCODING 'UTF-8'
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
     TEMPLATE template0;
