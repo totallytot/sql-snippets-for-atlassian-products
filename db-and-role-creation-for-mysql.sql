085b6291c792dbbc14a90be2bfe03e985888aa89
Totally Tot <fcukthepop@gmail.com>
2019-08-09 14:53:36 +0300

    Update db-and-role-creation-for-mysql.sql

--- db-and-role-creation-for-mysql.sql
+++ db-and-role-creation-for-mysql.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE <database-name> CHARACTER SET utf8 COLLATE utf8_bin;
+# jira
+CREATE DATABASE jiradb CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 GRANT ALL PRIVILEGES ON <database-name>.* TO '<confluenceuser>'@'localhost' IDENTIFIED BY '<password>';
