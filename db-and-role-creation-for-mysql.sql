f21ed5518d89398930e57e57cfc122f4d8349386
TotallyTot <fcukthepop@gmail.com>
2019-09-17 12:55:40 +0300

    sql for confluence

--- db-and-role-creation-for-mysql.sql
+++ db-and-role-creation-for-mysql.sql
@@ -1,3 +1,7 @@
 # jira MySql 5.7.6
 CREATE DATABASE jiradb CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 GRANT ALL PRIVILEGES ON <database-name>.* TO '<jirauser>'@'localhost' IDENTIFIED BY '<password>';
+
+# confluence Mysql 5.7
+CREATE DATABASE <database-name> CHARACTER SET utf8 COLLATE utf8_bin;
+GRANT ALL PRIVILEGES ON <database-name>.* TO '<confluenceuser>'@'localhost' IDENTIFIED BY '<password>';
