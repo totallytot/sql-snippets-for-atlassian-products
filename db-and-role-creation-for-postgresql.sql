ca5dbf3bda5798a8e9e4dc15e2dec63371bd78be
Totally Tot <fcukthepop@gmail.com>
2019-10-08 15:15:26 +0300

    sql snippet for bitbucket

--- db-and-role-creation-for-postgresql.sql
+++ db-and-role-creation-for-postgresql.sql
@@ -1,7 +1,9 @@
+-- Jira
 create role jira with password 'j76Tre!' login;
 CREATE DATABASE jiradb WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
 alter database jiradb owner to jira;
 
+-- Confluence
 create role confluence with password 'j76Tre!' login;
 CREATE DATABASE "confluence"
     WITH OWNER "confluence"
@@ -9,3 +11,7 @@ CREATE DATABASE "confluence"
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
     TEMPLATE template0;
+
+-- Bitbucket
+CREATE ROLE bitbucketuser WITH LOGIN PASSWORD 'jellyfish' VALID UNTIL 'infinity';
+CREATE DATABASE bitbucket WITH ENCODING='UTF8' OWNER=bitbucketuser CONNECTION LIMIT=-1;
