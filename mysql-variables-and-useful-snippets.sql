c4882d61d83deef33af8061f58f6af08940c360d
Totally Tot <fcukthepop@gmail.com>
2019-11-13 10:42:07 +0300

    Mysql variable added

--- mysql-variables-and-useful-snippets.sql
+++ mysql-variables-and-useful-snippets.sql
@@ -3,12 +3,15 @@ select @@innodb_log_file_size;
 select @@innodb_lock_wait_timeout;
 select @@max_connections;
 
---This should deliver you which Tables have references to the table you want to drop, 
---once you drop these references, or the datasets which reference datasets in this table you will be able to drop the table
+-- this should deliver you which Tables have references to the table you want to drop, 
+-- once you drop these references, or the datasets which reference datasets in this table you will be able to drop the table
 SELECT * FROM information_schema.KEY_COLUMN_USAGE 
 WHERE REFERENCED_TABLE_NAME = 'YourTable';
 
 SELECT table_name FROM information_schema.tables WHERE table_name ='Database Name';
 
--- Data folders
+-- data folders
 SHOW VARIABLES WHERE Variable_name LIKE '%dir';
+
+-- show connections
+SHOW PROCESSLIST;
