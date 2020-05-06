0fab320704bba2755bccaa3a1c804297a2b0e090
Totally Tot <fcukthepop@gmail.com>
2020-05-06 15:03:38 +0300

    Update db-and-role-creation-for-postgresql.sql

--- db-and-role-creation-for-postgresql.sql
+++ db-and-role-creation-for-postgresql.sql
@@ -1,7 +1,7 @@
 -- Jira
 create role jira with password 'j76Tre!' login;
-CREATE DATABASE jiradb WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
-alter database jiradb owner to jira;
+CREATE DATABASE jira WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
+alter database jira owner to jira;
 
 -- Confluence
 create role confluence with password 'j76Tre!' login;
