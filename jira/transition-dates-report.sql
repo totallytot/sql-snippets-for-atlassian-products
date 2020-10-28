bcf8262680a1d0c54f2ba9219628ef1c08593ed1
Totally Tot <fcukthepop@gmail.com>
2020-10-28 18:04:01 +0300

    Update transition-dates-report.sql

--- jira/transition-dates-report.sql
+++ jira/transition-dates-report.sql
@@ -1,8 +1,8 @@
-select concat(p.pkey,"-",ji.issuenum) as "Issue Key", it.PNAME as "Issue Type", ci.OLDSTRING As "From Status", 
+select concat(p.pkey,'-', ji.issuenum) as "Issue Key", it.PNAME as "Issue Type", ci.OLDSTRING As "From Status", 
 ci.NEWSTRING As "To Status", cg.created as "Transition Date", cg.AUTHOR from changeitem ci
 join changegroup cg on ci.GROUPID = cg.id
 join jiraissue ji on ji.id = cg.issueid
 join issuetype it on it.id = ji.ISSUETYPE
 join project p on p.id = ji.project
-where ci.field = 'status' and ci.newstring in ("TS Ordered", "CE Ordered")
-and p.pkey = "PELA" order by cg.created;
+where ci.field = 'status' and p.pkey = 'HAUFE' 
+order by ji.issuenum;
